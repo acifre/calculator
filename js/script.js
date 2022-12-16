4a8acda631251a330e7def9e0332d364496357db
acifre <78503928+acifre@users.noreply.github.com>
2022-12-16 16:16:59 -0600

    Fix subtract() logic to account for negative nums

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-    return num2 > num1 ? (num2 - num1) : (num1 - num2);
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
