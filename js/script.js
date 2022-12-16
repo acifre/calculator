3074278e55400196a66daaad3c314825e9253a96
acifre <78503928+acifre@users.noreply.github.com>
2022-12-16 14:28:15 -0600

    Write func for add, subtract, multi, divide
    Write operate() which switches through func cases

--- js/script.js
+++ js/script.js
@@ -2,7 +2,43 @@
 
 // 1. Create functions for:
 //       add, subtract, multiply, divide
+
+
+function add(num1, num2) {
+    return num1 + num2;
+
+}
+
+function subtract(num1, num2) {
+    return num2 > num1 ? (num2 - num1) : (num1 - num2);
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
 // 2. Create function operate
+
+function operate(func, num1, num2) {
+    switch (func) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+        default:
+            console.log("Error, invalid function")
+            return -999
+    }
+}
+
 // 3. Create functions that populate display
 // 4. Make calculator work
 
