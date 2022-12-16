aaebd5e94d64d477f42ed2caf625d0471e90d42c
acifre <78503928+acifre@users.noreply.github.com>
2022-12-16 00:08:50 -0600

    Add todo list to script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// TO DO
+
+// 1. Create functions for:
+//       add, subtract, multiply, divide
+// 2. Create function operate
+// 3. Create functions that populate display
+// 4. Make calculator work
+
