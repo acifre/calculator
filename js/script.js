a2277c4fd52ee0375c9e325ad8fdba9c5053084b
acifre <78503928+acifre@users.noreply.github.com>
2022-12-16 15:38:06 -0600

    Add equal button logic

--- js/script.js
+++ js/script.js
@@ -52,6 +52,7 @@ const displayText = document.querySelector(".item-display");
 
 displayText.innerHTML = '0';
 
+//OPERATOR LISTENERS
 const operators = document.querySelectorAll(".operator");
 
 for (let operator of operators) {
@@ -63,9 +64,60 @@ for (let operator of operators) {
     })
 }
 
+//NUMBER LISTENERS
+
+const numbers = document.querySelectorAll(".number");
+
+for (let number of numbers) {
+    number.addEventListener('click', () => {
+        if (number1 === 'empty' && number2 === 'empty') {
+            number1 = parseInt(number.innerHTML);
+            displayText.innerHTML = number.innerHTML;
+            console.log([1, number1, number2])
+        } else if (number1 != 'empty' && number2 === 'empty') {
+            number2 = parseInt(number.innerHTML);
+            displayText.innerHTML = number.innerHTML;
+            console.log([2, number1, number2])
+        } else {
+            displayText.innerHTML = 0;
+            alert("Error too many numbers.");
+            clear()
+        }
+    })
+}
+
+
+
 // add an event listener that on click 
 //updates the display with selection
 // store the selection in a variable num 1, num 2, operator 
 
 // 4. Make calculator work
+
+
+// CLEAR BUTTON
+
+const clearBtn = document.querySelector('.item-clear');
+
+clearBtn.addEventListener('click', clear);
+
+function clear() {
+    number1 = 'empty';
+    number2 = 'empty';
+    operatorFunction = 'empty';
+    displayText.innerHTML = 0;
+
+    console.log([number1, number2, operatorFunction]);
+}
+
+
+// EQUAL BUTTON
+
+const equalBtn = document.querySelector('.item-equal');
+
+equalBtn.addEventListener('click', () => {
+    let result = operate(operatorFunction, number1, number2);
+
+    displayText.innerHTML = result;
+});
 
