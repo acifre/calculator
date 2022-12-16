52e7c1dec308b847f172fa81c18164e5b1053cd7
acifre <78503928+acifre@users.noreply.github.com>
2022-12-16 17:29:53 -0600

    Add einstein quote when dividing 0 by 0

--- js/script.js
+++ js/script.js
@@ -7,25 +7,20 @@ let number2 = "empty";
 let operatorFunction = "empty";
 let display = 0;
 let result = 0;
-let equalPressed = false
+let equalPressed = false;
+let operatorPressed = false;
 
-// 1. Create functions for:
-//       add, subtract, multiply, divide
-
+// 1. Create functions for add, subtract, multiply, divide:     
 
 function add(num1, num2) {
     return num1 + num2;
-
 }
-
 function subtract(num1, num2) {
     return num1 - num2;
 }
-
 function multiply(num1, num2) {
     return num1 * num2;
 }
-
 function divide(num1, num2) {
     return num1 / num2;
 }
@@ -41,9 +36,13 @@ function operate(operator, num1, num2) {
         case "multiply":
             return multiply(num1, num2);
         case "divide":
+            if (num1 === 0 && num2 === 0) {
+                alert('Black holes are where God divided by zero. -Albert Einstein');
+                return 0;
+            }
             return divide(num1, num2);
         default:
-            console.log("Error, invalid function")
+            console.log('Error, invalid function')
             return -999
     }
 }
@@ -51,8 +50,7 @@ function operate(operator, num1, num2) {
 // 3. Create functions that populate display
 
 const displayText = document.querySelector(".item-display");
-
-displayText.innerHTML = '0';
+displayText.innerHTML = 0;
 
 //OPERATOR LISTENERS
 const operators = document.querySelectorAll(".operator");
@@ -60,9 +58,23 @@ const operators = document.querySelectorAll(".operator");
 for (let operator of operators) {
     operator.addEventListener('click', () => {
 
-        operatorFunction = operator.id;
-        displayText.innerHTML = operator.innerHTML;
-        equalPressed = false;
+        if (!operatorPressed) {
+            operatorFunction = operator.id;
+            displayText.innerHTML = operator.innerHTML;
+            equalPressed = false;
+            operatorPressed = true;
+        } else if (operatorPressed && number1 != 'empty' && number2 != 'empty') {
+            let oldOperator = operatorFunction;
+            result = +operate(oldOperator, parseInt(number1), parseInt(number2)).toFixed(8);
+            operatorFunction = operator.id;
+            number1 = result;
+            number2 = 'empty';
+            displayText.innerHTML = number1;
+        } else if (operatorPressed && number2 === 'empty') {
+            operatorFunction = operator.id;
+            displayText.innerHTML = operator.innerHTML;
+            equalPressed = false;
+        }
 
     })
 }
@@ -85,7 +97,7 @@ for (let number of numbers) {
             number2 = number.innerHTML;
             displayText.innerHTML = number.innerHTML;
             console.log([3, number1, number2])
-        } else if (number1 != 'empty' && operatorFunction != 'empty' && number2 != 'empty' && !equalPressed) {
+        } else if (number1 != 'empty' && operatorFunction != 'empty' && number2 != 'empty' && !equalPressed) { // !equalpressed so that numbers keep getting added to number2
             number2 += number.innerHTML;
             displayText.innerHTML = number2;
             console.log([4, number1, number2])
@@ -118,6 +130,8 @@ function clear() {
     number2 = 'empty';
     operatorFunction = 'empty';
     result = 0;
+    operatorPressed = false;
+    equalPressed = false;
     displayText.innerHTML = result;
 
     console.log([number1, number2, operatorFunction]);
@@ -129,11 +143,13 @@ function clear() {
 const equalBtn = document.querySelector('.item-equal');
 
 equalBtn.addEventListener('click', () => {
-    result = operate(operatorFunction, parseInt(number1), parseInt(number2));
+    result = +operate(operatorFunction, parseInt(number1), parseInt(number2)).toFixed(8);
 
     displayText.innerHTML = result;
     equalPressed = true;
     number1 = result;
     number2 = 'empty';
+
+    console.log([number1, number2, operatorFunction, operatorPressed, equalPressed])
 });
 
